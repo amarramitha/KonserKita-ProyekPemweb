fcb6c3e9ef893eba5ec7cac8bf3e131e98586f91
Amarramitha <amarra.mitha@gmail.com>
2024-05-30 18:06:13 +0700

    update navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="bg-brand-purple border-gray-200 dark:bg-gray-900 relative w-full">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+        <a href="/home" class="flex items-center space-x-3 rtl:space-x-reverse">
             <img src="{{ asset('storage/images/logo.png') }}" class="h-8" alt="KonserKita Logo" />
         </a>
 
