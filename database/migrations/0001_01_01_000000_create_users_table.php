fcfd8836995775ed8c65d4604e5004f0de6ada02
ivand <ivandarmawan2020@gmail.com>
2024-06-23 09:30:53 +0700

    database baru

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('google_id')->nullable()->after('email');
+            $table->string('google_id')->nullable();
             $table->string('google_token')->nullable();
             $table->string('google_refresh_token')->nullable();
             $table->enum('role', ['admin', 'user'])->default('user');
